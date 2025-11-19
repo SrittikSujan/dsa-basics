61392b1c36b63810859bcf1e97d93d679fc3c7ed
Srittik Sujan <sritiksujan.gg@gmail.com>
2025-11-19 18:37:41 +0530

    added createStack function

--- stack_using_linkedlist.c
+++ stack_using_linkedlist.c
@@ -7,8 +7,49 @@ typedef struct StackNode
     struct StackNode *next;
 } StackNode;
 
+StackNode *createStack(int data)
+{
+    StackNode *stack = (StackNode *)malloc(sizeof(StackNode));
+    stack->data = data;
+    stack->next = NULL;
+    return stack;
+}
+
+int isEmpty(StackNode *check)
+{
+    if (check == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+int isFull(StackNode *check)
+{
+    StackNode *temp = (StackNode *)malloc(sizeof(StackNode));
+    if(temp == NULL)
+        return 1;
+    else
+    {
+        free(temp);
+        return 0;
+    }
+}
+
 int main()
 {
+
+    int condition;
+    do
+    {
+        int data;
+        printf("Enter data to push onto the stack: ");
+        scanf("%d", &data);
+        createStack(data);
+
+        printf("Do you want to create another Stack Node? (1 for yes / 0 for no): ");
+        scanf("%d", &condition);
+    } while (condition == 1);
+    
 
     return 0;
 }
